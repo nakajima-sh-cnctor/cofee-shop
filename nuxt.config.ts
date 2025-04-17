4255cd62e8da730545c48f199d127c5928f811d5
Nakajima Shunsuke <nakajima.sh@cnctor.jp>
2025-04-17 11:28:02 +0900

    VuetifyをNuxtアプリケーションに統合し、必要な依存関係を追加。nuxt.config.tsにVuetifyの設定を追加し、vuetify.tsプラグインを作成。package.jsonとpackage-lock.jsonに関連パッケージを追加。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: [
+    '@nuxt/eslint',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
   devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
   compatibilityDate: '2024-11-01',
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   eslint: {
     config: {
       stylistic: true,
